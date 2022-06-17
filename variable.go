9f5f15477c3389fb1d038c8e810f472806155ff1
ShaikMahaboobSubani <107684991+ShaikMahaboobSubani@users.noreply.github.com>
2022-06-17 16:53:53 +0530

    variable

---
+++ variable.go
@@ -0,0 +1,33 @@
+package main
+
+//import "time"
+
+func main() {
+	//fmt.Println("subani")
+	println("subani")
+	//println(time.Now())
+
+	var num int
+	println("number is", num)
+
+	var num1 int16 = 88
+	println("number is", num1)
+
+	var decimal float32
+	println("float number is", decimal)
+
+	var ok bool
+	println("bool is", ok)
+	var bt byte
+	println("byte is", bt)
+	var rn rune
+	println("rune is", rn)
+
+	//var (
+	//number11     int    = 100
+	//name11       string = "subani"
+	//Ismarraige11 bool   = true
+	//)
+	//println("number11 : %d, name11: %s ,Ismarraige11 : %v \n", int.number11, string.name11, bool.Ismarraige11)
+
+}
